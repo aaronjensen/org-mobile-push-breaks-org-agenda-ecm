edc98f5d4212498395aa1a917b908250e47e1299
Aaron Jensen <aaronjensen@gmail.com>
2017-03-11 07:49:08 -0800

    Use org-plus-contrib

--- init.el
+++ init.el
@@ -3,12 +3,12 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (package-initialize)
-(unless package-archive-contents    ;; Refresh the packages descriptions
+(unless package-archive-contents
   (package-refresh-contents))
-(setq package-load-list '(all))     ;; List of packages to load
-(unless (package-installed-p 'org)  ;; Make sure the Org package is
-  (package-install 'org))           ;; installed, install it if not
-(package-initialize)                ;; Initialize & Install Package
+(setq package-load-list '(all))
+(unless (package-installed-p 'org-plus-contrib)
+  (package-install 'org-plus-contrib))
+(package-initialize)
 
 (setq create-lockfiles nil)
 (let ((d "~/.emacs.d"))
